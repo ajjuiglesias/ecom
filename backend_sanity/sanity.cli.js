8c47fb21552118af82b24b1334be80848fd06e7b
ajju <ajjuiglesias@gmail.com>
2023-02-17 23:04:03 +0530

    first commit

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'peqzjqjs',
+    dataset: 'production'
+  }
+})
---
+++ frontend_next/lib/client.js
@@ -0,0 +1,15 @@
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = createClient({
+  projectId: "peqzjqjs",
+  dataset: "production",
+  apiVersion: "2023-01-10",
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
